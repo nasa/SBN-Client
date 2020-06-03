b17c9ac882ed47dab002223d53b1d805dc7c00d7
Alan Gibson <alan.s.gibson@nasa.gov>
2020-06-03 18:07:37 -0400

    Clean up formatting. Add doxygen style commenting.

--- fsw/src/sbn_client_ingest.c
+++ fsw/src/sbn_client_ingest.c
@@ -1,23 +1,21 @@
 #include <pthread.h>
 
 #include "sbn_client_ingest.h"
-#include "sbn_client_utils.h"
 
-pthread_mutex_t receive_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t receive_mutex      = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t  received_condition = PTHREAD_COND_INITIALIZER;
 
-extern CFE_SBN_Client_PipeD_t PipeTbl[CFE_PLATFORM_SBN_CLIENT_MAX_PIPES];
-/* NOTE: Using memcpy to move message into pipe. What about pointer passing?
+/* TODO: Using memcpy to move message into pipe. What about pointer passing?
  *    Can we only look to msgId then memcpy only that then read directly
  *    into pipe? This could speed things up... */
-void ingest_app_message(int sockfd, SBN_MsgSz_t MsgSz)
+void ingest_app_message(int SockFd, SBN_MsgSz_t MsgSz)
 {
-    int status;
-    boolean at_least_1_pipe_in_use = FALSE;    
-    unsigned char msg_buffer[CFE_SB_MAX_SB_MSG_SIZE];
+    int            status, i;
+    boolean        at_least_1_pipe_is_in_use = FALSE;    
+    unsigned char  msg_buffer[CFE_SB_MAX_SB_MSG_SIZE];
     CFE_SB_MsgId_t MsgId;
     
-    status = CFE_SBN_CLIENT_ReadBytes(sockfd, msg_buffer, MsgSz);
+    status = CFE_SBN_CLIENT_ReadBytes(SockFd, msg_buffer, MsgSz);
     
     if (status != CFE_SUCCESS)
     {
@@ -26,6 +24,7 @@ void ingest_app_message(int sockfd, SBN_MsgSz_t MsgSz)
         snprintf(error_message, 60, 
           "CFE_SBN_CLIENT_ReadBytes returned a bad status = 0x%08X\n", status);
         log_message(error_message);
+        
         return;
     }
 
@@ -37,15 +36,13 @@ void ingest_app_message(int sockfd, SBN_MsgSz_t MsgSz)
     pthread_mutex_lock(&receive_mutex);
     
     /*Put message into pipe */    
-    int i;
-    
     for(i = 0; i < CFE_PLATFORM_SBN_CLIENT_MAX_PIPES; i++)
     {    
         if (PipeTbl[i].InUse == CFE_SBN_CLIENT_IN_USE)
         {
             int j;
             
-            at_least_1_pipe_in_use = TRUE;
+            at_least_1_pipe_is_in_use = TRUE;
             
             for(j = 0; j < CFE_SBN_CLIENT_MAX_MSG_IDS_PER_PIPE; j++)
             {
@@ -62,13 +59,17 @@ void ingest_app_message(int sockfd, SBN_MsgSz_t MsgSz)
                     }
                     else /* message is put into pipe */
                     {    
-                        // log_message("message received");
+                        log_message("App message received: MsgId 0x%08",
+                          MsgId);
+                        
                         memcpy(PipeTbl[i].Messages[message_entry_point(
                             PipeTbl[i])], msg_buffer, MsgSz);
                         PipeTbl[i].NumberOfMessages++;
                         
                         pthread_mutex_unlock(&receive_mutex);
-                        pthread_cond_signal(&received_condition); /* only a received message should send signal */
+                        /* only a received message should send signal */
+                        pthread_cond_signal(&received_condition);
+                        
                         return;
                     } /* end if */
                     
@@ -80,7 +81,7 @@ void ingest_app_message(int sockfd, SBN_MsgSz_t MsgSz)
     
     } /* end for */
     
-    if (at_least_1_pipe_in_use)
+    if (at_least_1_pipe_is_in_use)
     {
         log_message("SBN_CLIENT: ERROR no subscription for this msgid");  
     }
@@ -90,5 +91,4 @@ void ingest_app_message(int sockfd, SBN_MsgSz_t MsgSz)
     }
     
     pthread_mutex_unlock(&receive_mutex);
-    return;
 }
--- fsw/src/sbn_client_ingest.h
+++ fsw/src/sbn_client_ingest.h
@@ -2,7 +2,41 @@
 #define _sbn_client_ingest_h_
 
 #include "sbn_interfaces.h"
+#include "sbn_client_utils.h"
 
-void ingest_app_message(int, SBN_MsgSz_t);
 
+/**
+ * Extern reference to sbn client pipe table.
+ * Allows the message injest to fill the pipe
+ */
+extern CFE_SBN_Client_PipeD_t PipeTbl[CFE_PLATFORM_SBN_CLIENT_MAX_PIPES];
+ 
+ /****************** Function Prototypes **********************/
+ 
+ /** @defgroup SBNCLIENTIngest 
+  * @{
+  */
+ 
+ /*****************************************************************************/
+ /** 
+ ** \brief Receive an app message and direct it into pipe.
+ **
+ ** \par Description
+ **          This routine reads the given number of bytes from the given socket,
+ **          which becomes the message.  The message is then copied into the
+ **          correct pipe for the message's id.
+ **
+ ** \par Assumptions, External Events, and Notes:
+ **          The socket has been setup and receives app messages.
+ **
+ ** \param[in]  SockFd       A socket file descriptor that connects to the 
+ **                          that delivers app messages. 
+ **
+ ** \param[in]  MsgSz        The number of bytes to read for the message.
+ **
+ **/
+void ingest_app_message(int SockFd, 
+                         SBN_MsgSz_t MsgSz);
+ 
+ /**@}*/
 #endif /* _sbn_client_ingest_h_ */
